7860aa241e2a0c5acbab0e1c7a088bb6ad23d730
John Geliberte <gelibertejohn@gmail.com>
2019-08-31 11:55:42 +0800

    My first mobile app

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+ 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const showAlert = () => {
+  Alert.alert("SHOW ME");
+}
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignContent: 'center', padding: 20}}>
+      <View>
+        <Text style={{textAlign: 'center'}}>Hello John!</Text>
+        <TouchableOpacity style={{backgroundColor: 'red'}} onPress={()=> {showAlert()}}><Text>BEEP</Text></TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default App;
